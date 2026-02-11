0204198acb459ef6d99331455bcbed61c5175bb8
Nyan Linn Htet <shosuke48@gmail.com>
2026-02-11 17:10:47 +0630

    Register Updated

--- app/src/main/java/com/example/sampleapp/RegisterFragment.kt
+++ app/src/main/java/com/example/sampleapp/RegisterFragment.kt
@@ -6,7 +6,12 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import com.android.volley.Request
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
 import com.example.sampleapp.databinding.FragmentRegisterBinding
+import org.json.JSONObject
 
 class RegisterFragment : Fragment() {
     private lateinit var binding: FragmentRegisterBinding
@@ -16,12 +21,44 @@ class RegisterFragment : Fragment() {
     ): View? {
         binding = FragmentRegisterBinding.inflate(layoutInflater, container, false)
         binding.btnRegister.setOnClickListener {
+            val username = binding.editRegisUsername.text.toString()
             val email = binding.editRegisEmail.text.toString()
             val password = binding.editRegisPass.text.toString()
             val confirm = binding.editRegisConfirm.text.toString()
 
-            Log.i("Register","***$email, $password, $confirm")
+            if (password == confirm) {
+                val user = User(0, username, email, password)
+
+                Log.i("Register","***$username, $email, $password")
+                registerUser(user)
+            } else {
+                Toast.makeText(context, "Password does not match", Toast.LENGTH_LONG).show()
+            }
+
+
         }
         return binding.root
     }
-}
+
+    private fun registerUser(user: User) {
+        val url = "http://10.0.2.2/projects/mobileapi/register.php"
+
+        val request = object : StringRequest(Method.POST, url, { res ->
+            Log.i("Register Listener", "***Successfully Register")
+            val obj = JSONObject(res)
+
+            val msg = obj.get("message").toString()
+
+            Toast.makeText(context, "Response: $msg", Toast.LENGTH_LONG).show()
+        }, {error->
+
+            Log.i("Register Listener", "***Error Register:$error")
+        }) {
+            override fun getParams(): Map<String?, String?>? {
+                return mapOf("username" to user.username, "email" to user.email, "password" to user.password)
+            }
+        }
+        Volley.newRequestQueue(context).add(request)
+
+    } //Register
+} //RegisterFragment
