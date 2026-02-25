ad49db11576e633e20c9e3b30326948b0db3a1d6
Nyan Linn Htet <shosuke48@gmail.com>
2026-02-25 14:37:29 +0630

    Update

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -3,6 +3,7 @@ plugins {
     alias(libs.plugins.kotlin.android)
 
     id("androidx.navigation.safeargs.kotlin")
+    alias(libs.plugins.kotlin.compose)
 }
 
 android {
@@ -38,7 +39,8 @@ android {
         jvmTarget = "11"
     }
     buildFeatures {
-        viewBinding = true;
+        viewBinding = true
+        compose = true;
     }
 }
 
@@ -48,6 +50,13 @@ dependencies {
     implementation(libs.material)
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
+    implementation(libs.androidx.lifecycle.runtime.ktx)
+    implementation(libs.androidx.activity.compose)
+    implementation(platform(libs.androidx.compose.bom))
+    implementation(libs.androidx.compose.ui)
+    implementation(libs.androidx.compose.ui.graphics)
+    implementation(libs.androidx.compose.ui.tooling.preview)
+    implementation(libs.androidx.compose.material3)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
@@ -57,4 +66,8 @@ dependencies {
     implementation("androidx.navigation:navigation-dynamic-features-fragment:2.9.6")
 
     implementation("com.android.volley:volley:1.2.1")
+    androidTestImplementation(platform(libs.androidx.compose.bom))
+    androidTestImplementation(libs.androidx.compose.ui.test.junit4)
+    debugImplementation(libs.androidx.compose.ui.tooling)
+    debugImplementation(libs.androidx.compose.ui.test.manifest)
 }
--- build.gradle.kts
+++ build.gradle.kts
@@ -13,5 +13,6 @@ plugins {
 
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.kotlin.android) apply false
+    alias(libs.plugins.kotlin.compose) apply false
 }
 
