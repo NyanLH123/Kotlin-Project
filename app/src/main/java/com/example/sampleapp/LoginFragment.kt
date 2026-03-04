5faac2cedeea5b323aa53db8852422fc16213abf
Nyan Linn Htet <shosuke48@gmail.com>
2026-03-04 18:38:40 +0630

    LoginFragment updated

--- app/src/main/java/com/example/sampleapp/LoginFragment.kt
+++ app/src/main/java/com/example/sampleapp/LoginFragment.kt
@@ -1,12 +1,16 @@
 package com.example.sampleapp
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
 import com.example.sampleapp.databinding.FragmentLoginBinding
+import org.json.JSONObject
 
 class LoginFragment : Fragment() {
     private lateinit var binding: FragmentLoginBinding
@@ -21,14 +25,36 @@ class LoginFragment : Fragment() {
 
             if (email == "nyan@gmail.com" && password =="12345") {
                 Toast.makeText(context, "Login Successful", Toast.LENGTH_LONG).show()
+                Log.i("Register","***$email, $password")
+                loginUser(email, password)
             } else {
                 Toast.makeText(context, "Login Failed", Toast.LENGTH_LONG).show()
             }
         }
-        binding.btnLoginGoogle.setOnClickListener {
-
-        }
 
         return binding.root
+    }
+
+    private fun loginUser(email: String, password: String) {
+        val url = "http://10.0.2.2/projects/mobileapi/login.php"
+
+        val request =  object: StringRequest(Method.POST, url, {
+            res ->
+            Log.d("Login Listener","***Login Successful: $res")
+
+            val obj = JSONObject(res)
+            val msg = obj.getString("message")
+
+            Toast.makeText(context, "$msg", Toast.LENGTH_LONG).show()
+        }, {
+            error ->
+            Log.d("Login User", "***Login Error: $error")
+        }) {
+            override fun getParams(): Map<String?, String?>? {
+                return mapOf("email" to email, "password" to password)
+            }
+
+        }
+        Volley.newRequestQueue(context).add(request)
     }
 }
